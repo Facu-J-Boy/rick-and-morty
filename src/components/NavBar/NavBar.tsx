0d988140f0608ed28f313bf172bf33d1c1563888
Facu-J-Boy <boyfacundo45@gmail.com>
2024-11-18 17:34:49 -0300

    home with bootstrap

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,10 +4,10 @@ import styles from './NavBar.module.css'
 const NavBar = () => {
   return (
     <div className={styles.nav_bar}>
-        <input type='text' />
-        <button>
-            Login
-        </button>
+        <div className="input-group w-50 h-20 m-3">
+          <input type="text" className="form-control " placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" />
+        </div>
+        <button type="button" className="btn btn-dark">Login</button>
     </div>
   )
 }
